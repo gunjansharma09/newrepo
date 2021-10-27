6b0a56c7d7e4d0e9c1db8b4fee6d62bae9bbbd20
Gunjan Sharma <gunjan.sharma@tothenew.com>
2021-10-27 18:24:16 +0530

    models

--- src/main/java/com/example/ECommerce/model/Role.java
+++ src/main/java/com/example/ECommerce/model/Role.java
@@ -1,11 +1,14 @@
 package com.example.ECommerce.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.Set;
 
 @Entity
+@Data
 public class Role {
     @ManyToMany(mappedBy = "role")
     private Set<User> User;
--- src/main/java/com/example/ECommerce/model/User.java
+++ src/main/java/com/example/ECommerce/model/User.java
@@ -1,20 +1,21 @@
 package com.example.ECommerce.model;
-import lombok.Data;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "id" ,referencedColumnName = "id" )
     Set<Address> address;
@@ -38,6 +39,103 @@ public class User implements UserDetails {
     @NotEmpty
     @Column(name = "last_name")
     private String lastName;
+
+    public Set<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Set<Address> address) {
+        this.address = address;
+    }
+
+    public Set<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<Role> role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIs_Deleted() {
+        return is_Deleted;
+    }
+
+    public void setIs_Deleted(boolean is_Deleted) {
+        this.is_Deleted = is_Deleted;
+    }
+
+    public boolean isIs_Active() {
+        return is_Active;
+    }
+
+    public void setIs_Active(boolean is_Active) {
+        this.is_Active = is_Active;
+    }
+
+    public boolean isIs_Expired() {
+        return is_Expired;
+    }
+
+    public void setIs_Expired(boolean is_Expired) {
+        this.is_Expired = is_Expired;
+    }
+
+    public boolean isIs_Locked() {
+        return is_Locked;
+    }
+
+    public void setIs_Locked(boolean is_Locked) {
+        this.is_Locked = is_Locked;
+    }
+
+    public int getInvalid_Attempt_Count() {
+        return invalid_Attempt_Count;
+    }
+
+    public void setInvalid_Attempt_Count(int invalid_Attempt_Count) {
+        this.invalid_Attempt_Count = invalid_Attempt_Count;
+    }
+
     @NotEmpty
     private String password;
     private boolean is_Deleted;
@@ -46,34 +144,4 @@ public class User implements UserDetails {
     private boolean is_Locked;
     private int invalid_Attempt_Count;
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
